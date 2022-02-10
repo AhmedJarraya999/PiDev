76aed657fa749e18a88b4ab5efab0fd1932efcaa
AhmedJarraya999 <jarraya.ahmed@esprit.tn>
2022-02-10 02:26:16 +0100

    user entity commit

---
+++ src/com/esprit/entities/User.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.entities;
+
+/**
+ *
+ * @author jaray
+ */
+public class User {
+    private int id ;
+    private String name;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+    
+    
+}
